f25611680936ed5f2eba809ccefa2b34bbe2faaa
Joey Cho <cjy12182000@naver.com>
2019-01-22 05:13:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    
+    "My name is Joey"
   end
   
+  get '/hometown' do
+    "My hometown is Seoul"
+  end
   
+  get '/favorite-song' do
+    "My favorite-song is ignite"
+  end
 end
